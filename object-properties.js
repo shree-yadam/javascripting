fdd5f0cc5d2766dd3b07adb029f1a4ecffba50d3
Shree Devi Yadam <shree.yadam@gmail.com>
2021-05-09 23:39:00 +0000

    javascripting objects and functions

---
+++ object-properties.js
@@ -0,0 +1,26 @@
+/* JAVASCRIPTING OBJECT PROPERTIES
+
+## The challenge:  
+   
+  Create a file named object-properties.js.  
+   
+  In that file, define a variable named food like this:  
+   
+     const food = {  
+       types: 'only pizza'  
+     }  
+   
+  Use console.log() to print the types property of the food object to the  
+  terminal.  
+   
+  Check to see if your program is correct by running this command:  
+   
+     javascripting verify object-properties.js  
+
+*/
+
+const food = {  
+  types: 'only pizza'  
+};
+
+console.log(food.types);
---
+++ objects.js
@@ -0,0 +1,28 @@
+/* OBJECTS JAVASCRIPTING
+
+## The challenge:  
+   
+  Create a file named objects.js.  
+   
+  In that file, define a variable named pizza like this:  
+   
+     const pizza = {  
+       toppings: ['cheese', 'sauce', 'pepperoni'],  
+       crust: 'deep dish',  
+       serves: 2  
+     }  
+   
+  Use console.log() to print the pizza object to the terminal.  
+   
+  Check to see if your program is correct by running this command:  
+   
+     javascripting verify objects.js  
+*/
+
+const pizza = {
+  toppings : ['cheese', 'sauce', 'pepperoni'],  
+  crust: 'deep dish',  
+  serves: 2  
+};
+
+console.log(pizza);
